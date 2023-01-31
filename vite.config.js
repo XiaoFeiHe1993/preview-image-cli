d98fe4b9fb1e0b06edd02bfb2af684cc4205cf5d
xiaofeihe <1712776213@qq.com>
2023-01-31 20:54:31 +0800

    fix: 修改build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/process/dist',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        index: path.resolve(__dirname, 'index.js'),
+      },
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[ext]/[name].[ext]`,
+      },
+    },
+  },
 })
